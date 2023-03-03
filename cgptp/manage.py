171d976b4ae1a29f44ccfabdcb4d2f6c5c57b0db
Dmitry Mordanov <mordanov314@gmail.com>
2023-03-03 16:32:43 +0300

    stopped at 28:50

--- cgptp/manage.py
+++ cgptp/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cgptp.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taskmanager.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -22,3 +22,4 @@ if __name__ == '__main__':
     main()
 
 # https://www.youtube.com/watch?v=6K83dgjkQNw
+# 28:50
